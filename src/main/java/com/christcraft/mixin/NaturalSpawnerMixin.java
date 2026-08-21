0d7c98f682073d5291900793665a2525353d3854
fellow-11 <felixoshank@gmail.com>
2026-08-21 12:28:11 -0400

    untest

--- src/main/java/com/christcraft/mixin/NaturalSpawnerMixin.java
+++ src/main/java/com/christcraft/mixin/NaturalSpawnerMixin.java
@@ -7,7 +7,7 @@ import net.minecraft.world.level.NaturalSpawner;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import java.lang.Math;
-// test
+
 @Mixin(NaturalSpawner.class)
 public class NaturalSpawnerMixin {
 
