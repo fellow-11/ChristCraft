bbc82959d3962f625e9e0a33dcfbdd0f8cb4e10f
Ashes <ashdmy@outlook.com>
2026-08-21 14:22:12 -0400

    Refactor entity creation using static imports

--- src/main/java/com/christcraft/mixin/SwampHutPieceMixin.java
+++ src/main/java/com/christcraft/mixin/SwampHutPieceMixin.java
@@ -1,8 +1,8 @@
 package com.christcraft.mixin;
 
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntitySpawnReason;
-import net.minecraft.world.entity.EntityTypes;
+import static net.minecraft.world.entity.EntitySpawnReason.*;
+import static net.minecraft.world.entity.EntityTypes.*;
 import net.minecraft.world.entity.monster.Witch;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.levelgen.structure.structures.SwampHutPiece;
@@ -18,7 +18,7 @@ public class SwampHutPieceMixin {
                     target = "Lnet/minecraft/world/level/WorldGenLevel;addFreshEntityWithPassengers(Lnet/minecraft/world/entity/Entity;)V"))
     private void christcraft$replaceWitchOnAdd(WorldGenLevel level, Entity entity) {
         if (entity instanceof Witch witch) {
-            Entity replacement = EntityTypes.PILLAGER.create(level.getLevel(), EntitySpawnReason.STRUCTURE);
+            Entity replacement = PILLAGER.create(level.getLevel(), STRUCTURE);
             if (replacement != null) {
                 replacement.snapTo(witch.getX(), witch.getY(), witch.getZ(), witch.getYRot(), witch.getXRot());
                 level.addFreshEntityWithPassengers(replacement);
