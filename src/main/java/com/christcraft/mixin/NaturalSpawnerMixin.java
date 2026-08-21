bfafab8645a9767b622062bd5e8cfcf53f54a054
fellow-11 <felixoshank@gmail.com>
2026-08-21 11:59:02 -0400

    test

--- src/main/java/com/christcraft/mixin/NaturalSpawnerMixin.java
+++ src/main/java/com/christcraft/mixin/NaturalSpawnerMixin.java
@@ -1,26 +1,31 @@
 package com.christcraft.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import net.minecraft.util.random.Weighted;
-import net.minecraft.util.random.WeightedList;
-import net.minecraft.world.entity.EntityTypes;
+import static net.minecraft.world.entity.EntityTypes.*;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.NaturalSpawner;
-import net.minecraft.world.level.biome.MobSpawnSettings;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-
+import java.lang.Math;
+// test
 @Mixin(NaturalSpawner.class)
 public class NaturalSpawnerMixin {
 
-    @ModifyReturnValue(method = "mobsAt", at = @At("RETURN"))
-    private static WeightedList<MobSpawnSettings.SpawnerData> christcraft$swapFortressWither(
-            WeightedList<MobSpawnSettings.SpawnerData> original) {
+    private static final EntityType<?> REPLACEMENTS[] = {
+                    ENDERMAN
+    };
 
-        if (original.contains(new MobSpawnSettings.SpawnerData(EntityTypes.WITHER_SKELETON, 5, 5))) {
-            return WeightedList.<MobSpawnSettings.SpawnerData>of(
-                    new Weighted<>(new MobSpawnSettings.SpawnerData(EntityTypes.ENDERMAN, 1, 1), 1)
-            );
+    @ModifyReturnValue(method = "mobsAt", at = @At("RETURN"))
+    private static EntityType<?> christcraft$subMob (EntityType<?> fortressDefault) {
+
+        double index = Math.random();
+
+        if (fortressDefault == WITHER_SKELETON) {
+            index = Math.floor(index * REPLACEMENTS.length);
+            return REPLACEMENTS[(int)index];
+        } else {
+            return fortressDefault;
+
         }
-        return original;
     }
 }
